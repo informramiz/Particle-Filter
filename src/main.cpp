5b8b0d1c7a0e168cb5f59dbed54338f75bbd20a0
Ramiz Raja <informramiz@gmail.com>
2017-05-03 03:10:28 +0500

    feat: Created eclipse project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 3, 2017
+ *      Author: ramiz
+ */
+
+#include <iostream>
+
+int main(int argc, char* args[]) {
+  std::cout << "Hello world" << std::endl << std::endl;
+  return 0;
+}
+
+
